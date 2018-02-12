d3dd9ba0ceb12d514e2ee72739667277a60990b8
Allan Wazacz <cazwazacz@gmail.com>
2018-02-12 17:02:44 +0000

    Remove unused JS code

--- app/assets/javascripts/runners.js
+++ app/assets/javascripts/runners.js
@@ -1,7 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var button = document.getElementById('change-button');
 
-  button.addEventListener('click', function() {
-    document.getElementById('heading').innerText = 'wazap';
-  })
-})
