7716892e53cc360184cf56240682d6b0d3c37dcd
Allan Wazacz <cazwazacz@gmail.com>
2018-02-12 15:56:37 +0000

    Add user log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  # attr_accessible :email, :password, :password_confirmation
 
   attr_accessor :password
   before_save :encrypt_password
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
-  validates_presence of :email
+  validates_presence_of :email
   validates_uniqueness_of :email
 
   def self.authenticate(email, password)
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
 
   root 'runners#index'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
