470ce6b35f2243f39329a4509ee7127da842310f
Allan Wazacz <cazwazacz@gmail.com>
2018-02-12 17:00:18 +0000

    Add test for successful log in

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -14,3 +14,14 @@ feature 'Signing up' do
     expect(page).to have_content 'Signed up!'
   end
 end
+
+feature 'Logging in' do
+  scenario 'Logging in takes you to the index page which has the user\'s email' do
+    sign_up
+    click_link 'Log in'
+    fill_in 'email', with: 'allan@allan.com'
+    fill_in 'password', with: '123456'
+    click_button 'Log in'
+    expect(page).to have_content 'Logged in as allan@allan.com.'
+  end
+end
