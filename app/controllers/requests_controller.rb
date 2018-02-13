d82d601fda7ac238a7e8f7f49a5d9205d2ea9717
Allan Wazacz <cazwazacz@gmail.com>
2018-02-13 16:21:07 +0000

    Save request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,5 +1,21 @@
 class RequestsController < ApplicationController
   def create
-    p params["id"]
+    @request = Request.new(new_request_params)
+    @request.save
+    200
+  end
+
+  private
+
+  def user_1
+    params["id"].split("-")[0].to_i
+  end
+
+  def user_2
+    params["id"].split("-")[1].to_i
+  end
+
+  def new_request_params
+    { user_1: user_1, user_2: user_2, status: "pending" }
   end
 end
