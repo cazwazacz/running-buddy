8a24a08e3c2a143cac2c7ac7eba87ada170d6115
Allan Wazacz <cazwazacz@gmail.com>
2018-02-16 14:36:58 +0000

    Create test for a user to see a notification when a request they sent has been accepted

--- spec/features/requests_spec.rb
+++ spec/features/requests_spec.rb
@@ -15,6 +15,18 @@ feature 'Requests' do
     expect(page).to have_content 'Notifications (1)'
   end
 
+  scenario 'a user sees a notification a request they sent has been accepted' do
+    two_users_sign_up_and_log_in(true)
+    click_link 'Notifications (1)'
+    click_button 'Accept'
+    click_link 'Log out'
+    click_link 'Log in'
+    fill_in 'username', with: 'john'
+    fill_in 'password', with: '123456'
+    click_button 'Log in'
+    expect(page).to have_content 'Notifications (1)'
+  end
+
   scenario 'a user can see the requests they have sent and received' do
     two_users_sign_up_and_log_in(true)
     click_button 'Run with me!'
