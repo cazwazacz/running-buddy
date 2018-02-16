f6fc708f66613d4b47353f1018abf729524a9771
Allan Wazacz <cazwazacz@gmail.com>
2018-02-16 14:28:04 +0000

    Remove test

--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -4,12 +4,6 @@ describe User do
   let (:subject) { described_class.new }
   let (:request_double) { double('request_double', where: []) }
 
-  describe '#notifcations' do
-    it 'returns 0 when the length of the array is zero' do
-      expect(subject.notifications(request_double)).to eq 0
-    end
-  end
-
   describe '#requested?' do
     it 'returns true when the length of the array is greater than 0' do
       allow(request_double).to receive(:where).and_return([1])
